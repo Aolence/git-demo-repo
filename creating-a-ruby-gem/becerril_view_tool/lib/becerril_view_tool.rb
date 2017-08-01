ba021522f6c5dbc35dc51ab33e8c3513cc0babf2
Jordan Hudgens <jordan@devcamp.com>
2017-08-01 17:50:41 +0000

    Updated images for about me page

--- creating-a-ruby-gem/becerril_view_tool/lib/becerril_view_tool.rb
+++
@@ -1,6 +0,0 @@
-require "becerril_view_tool/version"
-require "becerril_view_tool/renderer"
-
-module BecerrilViewTool
-  # Your code goes here...
-end
--- creating-a-ruby-gem/becerril_view_tool/lib/becerril_view_tool/version.rb
+++
@@ -1,3 +0,0 @@
-module BecerrilViewTool
-  VERSION = "0.1.0"
-end
